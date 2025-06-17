80c5627e5b094417704376d2d81d8120b1951545
niic0o <nicosena10@gmail.com>
2025-06-17 15:50:54 -0300

    add favorite button

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,8 @@ class MyApp extends StatelessWidget {
         title: 'Hello Flutter',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 103, 174, 181)),
         ),
         home: MyHomePage(),
       ),
@@ -28,30 +29,42 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
   Color textColor = Colors.indigoAccent;
   Color bgColor = Colors.deepOrange.shade100;
 
   void getNext() {
-    
     current = WordPair.random();
 
     final random = Random();
-    
+
     textColor = Color.fromARGB(
       255, // opacidad máxima
       random.nextInt(256),
       random.nextInt(256),
       random.nextInt(256),
     );
-    
+
     bgColor = Color.fromARGB(
       255,
       random.nextInt(256),
       random.nextInt(256),
       random.nextInt(256),
     );
-    
-    notifyListeners();
+
+    notifyListeners(); //notifica a los widgets que hubo un cambio y toca hacer build de nuevo
+  }
+
+  //guardo una lista de palabras favoritas
+  var favorites = <WordPair>[];
+
+  void toggleFavorites() {
+    if (favorites.contains(current)){
+      favorites.remove(current);
+      }else{
+        favorites.add(current);
+      }
+      notifyListeners(); 
   }
 }
 
@@ -60,6 +73,13 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) { //esto asegura que pair no sea null.
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
     return Scaffold(
       body: Center(
@@ -72,12 +92,26 @@ class MyHomePage extends StatelessWidget {
                     color: appState.textColor,
                     backgroundColor: appState.bgColor)),
             BigCard(pair: pair), //objeto2
-            ElevatedButton(
-              //objeto3
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
+            SizedBox(height: 15),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  //objeto3
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: (){
+                    appState.toggleFavorites();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like')
+                  ),
+                  SizedBox(height: 15),
+              ],
             ),
           ],
         ),
@@ -94,17 +128,24 @@ class BigCard extends StatelessWidget {
 
   final WordPair pair;
 
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
 
-  final theme = Theme.of(context);
-
-  return Card(
-    color: theme.colorScheme.primary,
-    child: Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Text(pair.asLowerCase),
-    )
-  );
-}
+    return Card(
+        color: theme.colorScheme.primary,
+        elevation: 19.0, //sombreado
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            pair.asLowerCase,
+            style: style, //estilo de letras heredadas del tema
+            semanticsLabel:
+                "${pair.first} ${pair.second}", //permite a lectores de pantalla leer mejor las palabras
+          ),
+        ));
+  }
 }
