ef64388d2062e2226e965f92bf843ddaf513f7a3
niic0o <nicosena10@gmail.com>
2025-06-17 17:25:15 -0300

    add GeneratorPage and Change color selector

--- lib/main.dart
+++ lib/main.dart
@@ -38,19 +38,15 @@ class MyAppState extends ChangeNotifier {
 
     final random = Random();
 
-    textColor = Color.fromARGB(
-      255, // opacidad máxima
-      random.nextInt(256),
-      random.nextInt(256),
-      random.nextInt(256),
-    );
+// Generás un color aleatorio para el texto
+int r = random.nextInt(256);
+int g = random.nextInt(256);
+int b = random.nextInt(256);
 
-    bgColor = Color.fromARGB(
-      255,
-      random.nextInt(256),
-      random.nextInt(256),
-      random.nextInt(256),
-    );
+textColor = Color.fromARGB(255, r, g, b);
+
+// Calculás el color inverso para el fondo
+bgColor = Color.fromARGB(255, 255 - r, 255 - g, 255 - b);
 
     notifyListeners(); //notifica a los widgets que hubo un cambio y toca hacer build de nuevo
   }
@@ -58,67 +54,111 @@ class MyAppState extends ChangeNotifier {
   //guardo una lista de palabras favoritas
   var favorites = <WordPair>[];
 
-  void toggleFavorites() {
-    if (favorites.contains(current)){
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
       favorites.remove(current);
-      }else{
-        favorites.add(current);
-      }
-      notifyListeners(); 
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
   }
 }
 
+// ...
+
 class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
     IconData icon;
-    if (appState.favorites.contains(pair)) { //esto asegura que pair no sea null.
+    if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
     } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hola soy un texto centrado',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: appState.textColor,
-                    backgroundColor: appState.bgColor)),
-            BigCard(pair: pair), //objeto2
-            SizedBox(height: 15),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  //objeto3
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: (){
-                    appState.toggleFavorites();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like')
-                  ),
-                  SizedBox(height: 15),
-              ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: appState.bgColor, // Fondo más amplio
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+            child: Text(
+              ' Añade tu palabra favorita ',
+              style: TextStyle(
+                fontSize: 30,
+                color: appState.textColor,
+              ),
             ),
-          ],
-        ),
+          ),
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
