1807198315cbd4bae4afb868f2818a80fee1bbd0
niic0o <nicosena10@gmail.com>
2025-06-18 12:57:32 -0300

    add dependencie 'flutter_localization'

---
+++ lib/app_locale.dart
@@ -0,0 +1,31 @@
+/*
+  Frases que serán traducidas, se deben incluir en objetos (clases) y luego llamarlas desde
+  la pagina principal.
+*/
+
+mixin AppLocale {
+  static const String home = 'home';
+  static const String favorites = 'favorites';
+  static const String addFavorite = 'add_favorite';
+  static const String like = 'like';
+  static const String next = 'next';
+  static const String noFavorites = 'no_favorites';
+
+  static const Map<String, dynamic> en = {
+    home: 'Home',
+    favorites: 'Favorites',
+    addFavorite: 'Add your favorite word',
+    like: 'Like',
+    next: 'Next',
+    noFavorites: 'No favorites yet.',
+  };
+
+    static const Map<String, dynamic> es = {
+    home: 'Inicio',
+    favorites: 'Favoritos',
+    addFavorite: 'Añade tu palabra favorita',
+    like: 'Me gusta',
+    next: 'Siguiente',
+    noFavorites: 'Todavía no hay favoritos.',
+  };
+}
